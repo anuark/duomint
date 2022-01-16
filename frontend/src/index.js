68700a72cff721a805d301ec7850e43f28c77a07
Anuar <anuar.kilgore@buyersedgeplatform.com>
2022-01-15 22:12:13 -0600

    moved routing to App.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,39 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import Home from './views/Home/Home';
-import VoyagerSignUp from './views/VoyagerSignUp/VoyagerSignUp';
-import Pilot from './views/Pilot/Pilot';
-import Verify from './views/Verify/Verify';
-import Verify2 from './views/Verify2/Verify2';
-import Verify3 from './views/Verify3/Verify3';
-import Explore from './views/Explore/Explore';
-import ExploreOwnership from './views/Explore/ExploreOwnership';
-import ExploreUtility from './views/Explore/ExploreUtility';
-import ExploreAccess from './views/Explore/ExploreAccess';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './common/Header';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="/voyagers" element={<VoyagerSignUp />} />
-          <Route path="/pilots" element={<Pilot />} />
-          <Route path="/verify" element={<Verify />} />
-          <Route path="/verify2" element={<Verify2 />} />
-          <Route path="/verify3" element={<Verify3 />} />
-          <Route path="/explore" element={<Explore />} />
-          <Route path="/explore-ownership" element={<ExploreOwnership />} />
-          <Route path="/explore-utility" element={<ExploreUtility />} />
-          <Route path="/explore-access" element={<ExploreAccess />} />
-        </Route>
-      </Routes>
+        <App />
     </BrowserRouter>,
   </React.StrictMode>,
   document.getElementById('root')
